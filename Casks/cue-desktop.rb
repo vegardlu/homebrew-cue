9a9598128d013ef546a391ead0c924839e60c3bc
Vegard Lundeberg Hillestad <vegard.hillestad@gmail.com>
2026-09-26 11:04:51 +0200

    cue-desktop 0.11.0: appcast + cask

--- Casks/cue-desktop.rb
+++ Casks/cue-desktop.rb
@@ -1,6 +1,6 @@
 cask "cue-desktop" do
-  version "0.10.0"
-  sha256 "0228d0debc28db69516619cfd21cd8477e256bebe9af16615367beb0e615a041"
+  version "0.11.0"
+  sha256 "2a96cee6b572cc857418131cd803182274054861c27664b89feeabcd6068feb0"
 
   url "https://github.com/vegardlu/homebrew-cue/releases/download/cue-macos-v#{version}/Cue-#{version}.dmg",
       verified: "github.com/vegardlu/homebrew-cue/"
